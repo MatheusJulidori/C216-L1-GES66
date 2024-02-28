ee87ede97a7dac0c9c68e7fb3cddb14f0a0ae445
Matheus Julidori <matheus_julidori@yahoo.com.br>
2024-02-27 21:41:58 -0300

    Pratica 0

---
+++ pratica-0/src/index.js
@@ -0,0 +1,9 @@
+// Exemplo de uso prático
+let nome = "Matheus Julidori - GES 66";
+function saudacaoPersonalizada(nome) {
+    console.log("Olá, " + nome + "!");
+}
+saudacaoPersonalizada(nome);
+
+let modulo = require('./module.js');
+modulo.saudacao();
---
+++ pratica-0/src/module.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mensagem: 'Testando modulo: Matheus Julidori - GES 66 !',
+    saudacao: function() {
+      console.log(this.mensagem);
+    }
+  };
